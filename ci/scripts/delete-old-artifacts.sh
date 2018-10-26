82a80b25d76c5be063a0fe20808c9080a603423c
Brian Byers <brian@xclo.io>
2018-10-26 13:06:16 -0500

    swapping shell env

--- ci/scripts/delete-old-artifacts.sh
+++ ci/scripts/delete-old-artifacts.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
 minio-cleaner -host=${host} -access-key=${access-key} -secret-key=${secret-key} -prefix=${prefix} -bucket=${bucket} -backups-to-keep=${backups-to-keep} -use-ssl=${use-ssl} -dry-run=${dry-run}
