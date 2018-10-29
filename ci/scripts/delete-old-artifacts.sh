66bc06191c8dcedb69d90269c02622a55055dd13
Brian Byers <brian@xclo.io>
2018-10-29 10:38:29 -0500

    stripping out http and https schemes from URL

--- ci/scripts/delete-old-artifacts.sh
+++ ci/scripts/delete-old-artifacts.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
+#strip http/https
+host="$(echo $host | sed 's~^http[s]*://~~')"
+
 minio-cleaner -host=${host} -access-key=${access_key} -secret-key=${secret_key} -prefix=${prefix} -bucket=${bucket} -backups-to-keep=${backups_to_keep} -use-ssl=${use_ssl} -dry-run=${dry_run}
